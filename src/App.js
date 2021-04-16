b76fa851a118161ca2f3d7b8bc1c255854329624
Olaiwola Bolaji <olaiwola.bolaji@gmail.com>
2021-04-16 21:38:03 +0100

    feat: list users with expense details

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+  
+import React, { Component } from 'react';
+import HomePageContainer from './containers/HomePageContainer';
+import { connect } from 'react-redux';
+import NavPanel from './NavPanel';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavPanel/>
+        <Router>
+          <div>
+            <Route exact path="/" component={HomePageContainer} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+export default connect()(App);
